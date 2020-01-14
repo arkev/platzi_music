c0a8a66a63421fe514dbd6ae189b486d65bdaef5
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2020-01-13 23:47:56 -0600

    carga de archivos

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+
+  data (){
+    return{
+      msg: 'Hola mundo!'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
